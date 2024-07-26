86dfd77a015376b736b964a485b0b8c5dfebb1c7
Graciliana Kascher <eagraciliana@gmail.com>
2024-07-26 18:46:03 -0300

    [feat] creating style gradient of button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,45 +4,53 @@ import { features } from "process";
 export default function Home() {
   return (
     <div className="flex items-center justify-center py-8">
-      <section className="w-1/2 h-auto bg-slate-600 rounded-xl ">
-        <h1 className="text-5xl text-white font-bold pt-12 ml-11 ">
-          Faça o seu login
+      <section className="w-1/2 h-auto bg-[#070709] rounded-xl ">
+        <h1 className="text-5xl ml-11 text-white font-bold pt-12 ">
+          Faça o seu login{" "}
+          <span className="rounded-full  inline-block w-3 h-3 bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70] ml-2"></span>
         </h1>
         <form action="#">
           <div className="mx-11 mt-10">
             <label className="font-normal mt-10 text-sm text-gray-400">
               Email
             </label>
-            <input
-              type="email"
-              id="email"
-              className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="nome@email.com"
-              required
-            />
+            <div className="bg-none focus-within:bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70]  ">
+              
+              <input
+                type="email"
+                id="email"
+                className="  text-gray-800 text-sm rounded-lg  outline-none appearance-none w-full p-2.5"
+                placeholder="nome@email.com"
+                required
+              />
+            </div>
           </div>
           <div className="mx-11 mt-6">
             <label className="font-normal mt-10 text-sm text-gray-400">
               Senha
             </label>
-            <input
+            <div className="bg-none focus-within:bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70]  ">
+               <input
               type="password"
               id="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              className="text-gray-800 text-sm rounded-lg outline-none appearance-none w-full p-2.5"
               placeholder="•••••••••"
               required
-            />
+              />
+              
+            </div>
+           
           </div>
           <a className="font-normal flex justify-end underline mx-11 mt-4 text-sm text-gray-400">
             Esqueci minha senha
           </a>
           <div className="mx-11 mt-6">
-            <button className="bg-green-500 w-full font-bold text-2xl rounded-xl h-12 text-white">
+            <button className="bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70] w-full font-bold text-2xl rounded-xl h-12 text-white">
               Entrar
             </button>
           </div>
 
-          <a className="font-normal flex justify-center underline mx-11 mt-8 text-sm mb-10 text-gray-400">
+          <a className="font-normal flex justify-center underline mx-11 mt-8 text-sm mb-10 text-gray-400 drop-shadow-md md:drop-shadow-xl">
             Ainda não tenho uma conta
           </a>
         </form>
